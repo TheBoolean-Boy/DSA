37a1520749632536d7e79b840c1e176e386f83cb
Saswat Rath <94009506+TheBoolean-Boy@users.noreply.github.com>
2025-01-25 16:39:01 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (62.67%) - LeetSync

--- 289-game-of-life/game-of-life.cpp
+++ 289-game-of-life/game-of-life.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
     void gameOfLife(vector<vector<int>>& board) {
-        int n = board.size();
-        int m = board[0].size();
-
-        for(int i = 0; i < n; i++){
-            for(int  j = 0; j < m; j++){
+        int m = board.size();
+        int n = board[0].size();
+        for(int i = 0; i < m ; i++){
+            for(int j = 0; j< n; j++){
                 int alive = 0;
-                if(i > 0 && abs(board[i-1][j]) == 1)alive++;
-                if(i < n-1 && abs(board[i+1][j]) == 1)alive++;
-                if(j > 0 && abs(board[i][j-1]) == 1)alive++;
-                if(j < m-1 && abs(board[i][j+1]) == 1) alive++;
-                if(i > 0 && j < m-1 && abs(board[i-1][j+1]) == 1)alive++;
+                if(j < n-1 && abs(board[i][j+1]) == 1)alive++;
+                if(i < m-1 && abs(board[i+1][j]) == 1)alive++;
+                if(i < m-1 && j < n-1 && abs(board[i+1][j+1]) == 1)alive++;
+                if(i > 0 && j < n-1 && abs(board[i-1][j+1]) == 1)alive++;
+                if(i > 0 && abs(board[i-1][j]) == 1 )alive++;
                 if(i > 0 && j > 0 && abs(board[i-1][j-1]) == 1)alive++;
-                if(i < n-1 && j < m-1 && abs(board[i+1][j+1]) == 1)alive++;
-                if(j > 0 && i < n-1 && abs(board[i+1][j-1]) == 1)alive++;
-
+                if(j > 0 && abs(board[i][j-1]) == 1)alive++;
+                if(i < m-1 && j > 0 && abs(board[i+1][j-1]) == 1)alive++;
 
                 if(board[i][j] == 1){
-                    if(alive > 3 || alive < 2){
-                    board[i][j] = -1;
+                    if(alive < 2 || alive > 3){
+                        board[i][j] = -1;
+                    }
                 }
-                }
-                if(board[i][j] == 0){
+                else if(board[i][j] == 0){
                     if(alive == 3){
                         board[i][j] = 2;
                     }
                 }
             }
         }
-         for(int i = 0; i < n; i++){
-            for(int  j = 0; j < m; j++){
+
+        for(int i = 0 ; i < m; i++){
+            for(int j = 0; j < n; j++){
                 if(board[i][j] == 2)board[i][j] = 1;
-                else if(board[i][j] == -1)board[i][j] = 0;
+                if(board[i][j] == -1)board[i][j] = 0;
             }
         }
     }
