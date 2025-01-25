f8dc0ed5b3f57c91608fee3a278f1b7b59daa9ca
Saswat Rath <94009506+TheBoolean-Boy@users.noreply.github.com>
2025-01-25 08:44:18 +0530

    Time: 0 ms (100.00%) | Memory: 25.5 MB (95.19%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        long long sz = grid.size();
-        long long N = sz*sz;
-        long long SN = (N *(N + 1))/2;
-        long long S2N = (N * (N + 1) * (2*N +1))/6;
+        long long n = grid.size();
+        long long N = n*n;
+        long long sn = N* (N+1)/2;
+        long long s2n = N * (N+1) * (2*N + 1)/6;
 
-        long long S = 0, S2 =0;
-        for(int i = 0; i < sz; i++){
-            for(int j =0; j< sz; j++){
-                S += grid[i][j];
-                S2 += (long long)grid[i][j] * (long long)grid[i][j];
+        long long sum = 0, sum2sq = 0;
+
+        for(int i = 0; i <n; i++){
+            for (int j = 0; j < n; j++){
+                sum += grid[i][j];
+                sum2sq += grid[i][j] * grid[i][j];
             }
         }
 
-        long long val1 = S - SN;
-        long long val2 = S2 - S2N;
+        long long val1 = sum - sn;
+        long long val2 = sum2sq - s2n;
+
         val2 = val2/val1;
 
-        long long x = (val1 + val2)/2;
-        long long y = val2 - x;
+        long long a = (val1+val2)/2;
+        long long b = val2 - a;
 
-        return{(int)x, (int)y};
+        return {(int)a, (int)b};
     }
 };
