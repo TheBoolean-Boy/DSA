b191d50154a8f722d2cb0ede9cc9346ade41bb43
Saswat Rath <94009506+TheBoolean-Boy@users.noreply.github.com>
2025-01-29 11:56:25 +0530

    Time: 7 ms (76.47%) | Memory: 26.1 MB (81.28%) - LeetSync

---
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checker(vector<int>&nums, int divisor, int threshold){
+        long long sum = 0;
+        for(int i = 0; i< nums.size(); i++){
+            sum += ceil((double)nums[i] / (double)divisor);
+        }
+        if(sum <= threshold)return true;
+        return false;
+    }
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int low = 1;
+        int high = *max_element(nums.begin(), nums.end());
+        while(low <= high){
+            int mid =  low + (high - low)/2;
+            bool check = checker(nums, mid, threshold);
+            if(check == true){
+                high = mid - 1;
+            }
+            else{
+                low  = mid + 1;
+            }
+        }
+        return low;
+    }
+};
