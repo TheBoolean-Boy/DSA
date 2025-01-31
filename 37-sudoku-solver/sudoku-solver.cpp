9a6cf756c455262886daa96be21cfc03459a92a3
Saswat Rath <94009506+TheBoolean-Boy@users.noreply.github.com>
2025-01-31 11:08:25 +0530

    Time: 481 ms (23.03%) | Memory: 8.8 MB (29.73%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        solver(board);
+    }
+    bool solver(vector<vector<char>>&board){
+        for(int row = 0; row < board.size(); row++){
+            for(int col = 0; col < board[0].size(); col++){
+
+                if(board[row][col] == '.'){
+                    for(char c =  '1'; c <= '9'; c++){
+                        if(isPossible(board, row, col, c)){
+                            board[row][col] = c;
+
+                            if(solver(board) == true)return true;
+                            else board[row][col]= '.';
+            
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    bool isPossible(vector<vector<char>>&board, int row, int col, int value){
+        for(int i =  0; i < 9; i++){
+            if(board[row][i] == value)return false;
+
+            if(board[i][col] == value)return false;
+
+            if(board[3 * (row/3) + (i/3)][3 * (col/3) + (i%3)] == value)return false;
+        }
+        return true;
+    }
+};
