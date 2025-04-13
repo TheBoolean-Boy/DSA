5231a506a5c68c050cf862f9d06dc6b60ffed529
Saswat Rath <94009506+TheBoolean-Boy@users.noreply.github.com>
2025-04-14 01:03:36 +0530

    Time: 0 ms (100.00%) | Memory: 95.1 MB (62.86%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,45 +1,39 @@
-const auto _ = std::cin.tie(nullptr)->sync_with_stdio(false);
-
-#define LC_HACK
-#ifdef LC_HACK
-const auto __ = []() {
-    struct ___ {
-        static void _() { std::ofstream("display_runtime.txt") << 0 << '\n'; }
-    };
-    std::atexit(&___::_);
-    return 0;
-}();
-#endif
-
-
-#define pb push_back
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-        if(n1 > n2)return findMedianSortedArrays(nums2, nums1);
-        int low = 0 , high = n1;
-        int n = n1 + n2;
-        int left = (n1 + n2 + 1)/2;
-        while(low <= high){
-            int mid1 = (low + high) >> 1;
-            int mid2  = left - mid1;
-            int l1 = INT_MIN, l2 = INT_MIN;
-            int r1 = INT_MAX, r2 = INT_MAX;
-            if(mid1 < n1)r1 = nums1[mid1];
-            if(mid2 < n2)r2 = nums2[mid2];
-            if(mid1 > 0)l1 = nums1[mid1 -1];
-            if(mid2 > 0)l2 = nums2[mid2 -1];
-            if(l1 <= r2 && l2 <= r1){
-                if(n%2 == 1)return max(l1, l2);
-                return (double)(max(l1, l2) + min(r1, r2))/2.0;
-            }
-            if(l1 > r2)high = mid1 - 1;
-            else{
-                low = mid1 + 1;
+         if (nums1.size() > nums2.size()) {
+            swap(nums1, nums2);
+        }
+        
+        int m = nums1.size();
+        int n = nums2.size();
+        int low = 0, high = m;
+        
+        while (low <= high) {
+            int partitionX = (low + high) / 2;
+            int partitionY = (m + n + 1) / 2 - partitionX;
+            
+            // Handle edge cases where partitionX is 0 or m, partitionY is 0 or n
+            int maxLeftX = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];
+            int minRightX = (partitionX == m) ? INT_MAX : nums1[partitionX];
+            
+            int maxLeftY = (partitionY == 0) ? INT_MIN : nums2[partitionY - 1];
+            int minRightY = (partitionY == n) ? INT_MAX : nums2[partitionY];
+            
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                if ((m + n) % 2 == 0) {
+                    return (max(maxLeftX, maxLeftY) + min(minRightX, minRightY)) / 2.0;
+                } else {
+                    return max(maxLeftX, maxLeftY);
+                }
+            } else if (maxLeftX > minRightY) {
+                high = partitionX - 1;
+            } else {
+                low = partitionX + 1;
             }
         }
-        return 1.0;
+        
+        // In case the input arrays are not sorted or other error
+        throw invalid_argument("Input arrays are not sorted or other error");
     }
 };
